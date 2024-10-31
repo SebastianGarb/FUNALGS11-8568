b94cb05a27fd4432eeb7e477825e403c10218d04
SebastianGarb <sebastiangarboza63@hotmial.com>
2024-10-31 12:17:57 -0500

    Commits

---
+++ semana12/Program.cs
@@ -0,0 +1,48 @@
+﻿using semana12;
+using System.Runtime.Serialization.Formatters;
+
+byte opcion;
+string regresar;
+estudiantes e = new estudiantes();
+
+do
+{
+    Console.WriteLine("BIENVENIDO AL SISTEMA DE REGISTRO DE EDADES\n ");
+    Console.WriteLine("******* MENÚ *******");
+    Console.WriteLine("* 1. Insertar      *");
+    Console.WriteLine("* 2. Mostrar       *");
+    Console.WriteLine("* 3. Eliminar      *");
+    Console.WriteLine("* 4. Ordenar       *");
+    Console.WriteLine("* 0. Salir         *");
+    Console.WriteLine("********************");
+    Console.Write("\nIngrese opcion: ");
+    while (!byte.TryParse(Console.ReadLine(), out opcion) || opcion > 4)
+    {
+        Console.Write("Error: Ingrese opcion: ");
+    }
+
+    switch (opcion)
+    {
+        case 0:
+            Environment.Exit(0);
+            break;
+        case 1:
+            e.insertar();
+            break;
+        case 2:
+            e.mostrar();
+            break;
+        case 3:
+            e.mostrar();
+            Console.WriteLine("\nIngrese edad a eliminar: ");
+            byte ed = byte.Parse(Console.ReadLine());
+            e.eliminar(ed);
+            break;
+        case 4:
+
+            break;
+    }
+    Console.WriteLine("\n\nDesea regresar al menu? (si)");
+    regresar = Console.ReadLine();
+    Console.Clear();    
+} while (regresar == "si");
---
+++ semana12/estudiantes.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace semana12
+{
+    internal class estudiantes
+    {
+        byte[] edades = new byte[0];
+        int posicion = 0;
+        string seguir;
+
+        public void insertar()
+        {
+            byte ed;
+            do
+            {
+                Console.Write("\nIngrese edad: ");
+                while (!byte.TryParse(Console.ReadLine(), out ed) || ed < 14 || ed > 120)
+                {
+                    Console.Write("Error: Ingrese edad: ");
+                }
+                Array.Resize(ref edades, edades.Length + 1);
+                edades[posicion] = ed;
+                posicion++;
+                Console.WriteLine("\nEdad registrado correcatamente ");
+                Console.WriteLine("\nDesea seguir registrando [si] ");
+                seguir = Console.ReadLine();
+            } while (seguir == "si");
+        }
+        public void mostrar()
+        {
+            Console.SetCursorPosition(10, 12);
+            Console.Write("POSICION");
+            Console.SetCursorPosition(30, 12);
+            Console.Write("EDADES");
+            for (int i = 0; i < edades.Length; i++)
+            {
+                Console.SetCursorPosition(10, 13 + i);
+                Console.Write(i);
+                Console.SetCursorPosition(30, 13 + i);
+                Console.Write(edades[i]);
+            }
+        }
+        public void eliminar(byte ed)
+        {
+            int indice = Array.IndexOf(edades, ed);
+            
+            if (indice != -1)
+            {
+                for (int i = indice; i < edades.Length -1; i++)
+                {
+                    edades [i] = edades[i+1];
+                }
+                Array.Resize(ref edades, edades.Length - 1);
+                posicion--;
+                Console.WriteLine("\nEdad elmininado correcatamente");
+            }
+            else
+            {
+                Console.WriteLine("\nNo se puede eliminar porque no existe");
+            }
+        }
+        public void ordenar()
+        {
+
+        }
+    }
+}
